ac51d0bec6d3ba2d5a3e716aaec0f57a3a83806b
兰小兰 <479323417@qq.com>
2019-11-30 20:24:49 +0800

    初始工作-解决后端返回数据超过安全整数范围

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
+import JSONbig from 'json-bigint'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
 })
+
+request.defaults.transformResponse = [function (data) {
+  try {
+    return JSONbig.parse(data)
+  } catch (err) {
+    return {}
+  }
+}]
 
 export default request
