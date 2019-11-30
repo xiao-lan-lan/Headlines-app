099e6d49aab1e5fc543fabf3703c5dc41427913c
兰小兰 <479323417@qq.com>
2019-11-30 20:39:47 +0800

    登录-配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 路由组件
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
--- src/utils/request.js
+++ src/utils/request.js
@@ -5,6 +5,7 @@ const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
 })
 
+// 解决后端返回数据超过安全整数范围
 request.defaults.transformResponse = [function (data) {
   try {
     return JSONbig.parse(data)
