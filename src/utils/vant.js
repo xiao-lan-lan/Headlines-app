000612c907183ece1cb5dcbe713dcca77431d7c0
兰小兰 <479323417@qq.com>
2019-11-30 21:14:40 +0800

    登录-结构&样式

--- src/utils/vant.js
+++ src/utils/vant.js
@@ -1,6 +1,19 @@
 // 按需引入vant组件,创建单独文件存放引入并注册vant组件,都在main.js中不易维护
 
 import Vue from 'vue'
-import { Button } from 'vant'
+import {
+  Button,
+  NavBar,
+  Cell,
+  CellGroup,
+  Field,
+  Icon
+} from 'vant'
 
-Vue.use(Button)
+Vue
+  .use(Button)
+  .use(NavBar)
+  .use(Cell)
+  .use(CellGroup)
+  .use(Field)
+  .use(Icon)
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,15 +1,48 @@
 <template>
   <div class="login">
-      登录组件
+    <!-- 顶部导航 -->
+    <van-nav-bar title="登录" />
+
+    <!-- 表单 -->
+    <van-cell-group>
+      <van-field clearable left-icon="contact" placeholder="请输入手机号" />
+      <van-field center clearable placeholder="请输入验证码" left-icon="closed-eye">
+        <van-button slot="button" size="small" type="primary" round >获取验证码</van-button>
+      </van-field>
+    </van-cell-group>
+
+    <!-- 登录按钮 -->
+    <div class="button">
+      <van-button disabled type="info">登录</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Login-Page'
 }
 </script>
 
-<style>
-
+<style lang="less">
+.login {
+    .van-nav-bar {
+        background-color: #3296fa;
+        .van-nav-bar__title {
+            color: #fff
+        }
+    }
+    .van-button--primary {
+        background-color: #ededed;
+        border-color: #ededed;
+        color: rgba(49, 48, 48, 0.733);
+        font-size: 14px;
+    }
+    .button {
+        padding: 30px 15px;
+        .van-button {
+            width: 100%;
+        }
+    }
+}
 </style>
