f08769b636f6467b60af5dede7c11e82239f1c98
兰小兰 <479323417@qq.com>
2019-12-04 17:45:33 +0800

    搜索结果页-布局结构

--- src/views/search/SearchResults.vue
+++ src/views/search/SearchResults.vue
@@ -1,13 +1,44 @@
 <template>
-  <div class="box">搜索结果</div>
+  <div class="box">
+    <!-- 顶部导航标题 -->
+    <van-nav-bar title="xxx的搜索结果" left-arrow @click-left="$router.back()" />
+
+    <!-- 搜索结果列表 -->
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <van-cell v-for="SearchResult in SearchResults" :key="SearchResult" :title="SearchResult" />
+    </van-list>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'SearchResults',
+  data () {
+    return {
+      SearchResults: [],
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.SearchResults.push(this.SearchResults.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
 
+        // 数据全部加载完成
+        if (this.SearchResults.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
