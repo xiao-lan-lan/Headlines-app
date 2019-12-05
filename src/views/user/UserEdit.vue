4cc621a51403114aae966225a44b044877c2387d
兰小兰 <479323417@qq.com>
2019-12-05 21:08:27 +0800

    用户信息修改页-布局结构

--- src/views/user/UserEdit.vue
+++ src/views/user/UserEdit.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="user">
-      用户信息修改页面
+    <!-- 顶部导航标题 -->
+    <van-nav-bar title="个人信息" left-arrow right-text="保存" />
+
+    <!-- 编辑项 -->
+    <van-cell-group>
+      <van-cell title="头像" is-link value="内容">
+        <van-image
+          slot="default"
+          width="40"
+          height="40"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+      </van-cell>
+      <van-cell title="昵称" is-link value="内容">
+        <span slot="default"></span>
+      </van-cell>
+      <van-cell title="性别" is-link value="内容">
+        <span slot="default"></span>
+      </van-cell>
+      <van-cell title="生日" is-link value="内容">
+        <span slot="default"></span>
+      </van-cell>
+    </van-cell-group>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
