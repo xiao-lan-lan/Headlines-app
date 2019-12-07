978c69f51ead632ab2a78cf50c960c57699dd690
兰小兰 <479323417@qq.com>
2019-12-08 01:14:37 +0800

    我的页面-未登录状态

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -6,7 +6,7 @@
       <van-tabbar-item to="/" icon="wap-home-o">主页</van-tabbar-item>
       <van-tabbar-item to="/qa" icon="more-o">问答</van-tabbar-item>
       <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
-      <van-tabbar-item to="/user" icon="user-circle-o">我的</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="user-circle-o">{{$store.state.TokenObj?'我的':'未登录'}}</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -1,45 +1,58 @@
 <template>
   <div class="user" style="padding:10px">
-    <!-- 用户名头像 -->
-    <van-cell :title="UserData.name" is-link style="margin-bottom:10px" @click="$router.push('/my')">
-      <van-image
-        width="50"
-        height="50"
-        :src="UserData.photo"
-        slot="icon"
-        style="margin-right:15px"
-      />
-    </van-cell>
+    <!-- 登录圆环 -->
+    <div class="nologin" v-if="!$store.state.TokenObj">
+      <div class="circle" @click="$router.push('/login')">登录</div>
+    </div>
 
-    <!-- 关注信息 -->
-    <van-grid :border="false">
-      <van-grid-item icon="photo-o" text="头条">
-        <span slot="icon">{{UserData.art_count}}</span>
-      </van-grid-item>
-      <van-grid-item icon="photo-o" text="关注">
-        <span slot="icon">{{UserData.follow_count}}</span>
-      </van-grid-item>
-      <van-grid-item icon="photo-o" text="粉丝">
-        <span slot="icon">{{UserData.fans_count}}</span>
-      </van-grid-item>
-      <van-grid-item icon="photo-o" text="获赞">
-        <span slot="icon">{{UserData.like_count}}</span>
-      </van-grid-item>
-    </van-grid>
+    <!-- 登录用户信息 -->
+    <div class="login" v-else>
+      <!-- 用户名头像 -->
+      <van-cell
+        :title="UserData.name"
+        is-link
+        style="margin-bottom:10px"
+        @click="$router.push('/my')"
+      >
+        <van-image
+          width="50"
+          height="50"
+          :src="UserData.photo"
+          slot="icon"
+          style="margin-right:15px"
+        />
+      </van-cell>
 
-    <!-- 我的关注 -->
-    <van-grid>
-      <van-grid-item icon="star" text="我的收藏" />
-      <van-grid-item icon="chat" text="我的评论" />
-      <van-grid-item icon="like" text="我的点赞" />
-      <van-grid-item icon="browsing-history" text="浏览历史" />
-    </van-grid>
+      <!-- 关注信息 -->
+      <van-grid :border="false">
+        <van-grid-item icon="photo-o" text="头条">
+          <span slot="icon">{{UserData.art_count}}</span>
+        </van-grid-item>
+        <van-grid-item icon="photo-o" text="关注">
+          <span slot="icon">{{UserData.follow_count}}</span>
+        </van-grid-item>
+        <van-grid-item icon="photo-o" text="粉丝">
+          <span slot="icon">{{UserData.fans_count}}</span>
+        </van-grid-item>
+        <van-grid-item icon="photo-o" text="获赞">
+          <span slot="icon">{{UserData.like_count}}</span>
+        </van-grid-item>
+      </van-grid>
+
+      <!-- 我的关注 -->
+      <van-grid>
+        <van-grid-item icon="star" text="我的收藏" />
+        <van-grid-item icon="chat" text="我的评论" />
+        <van-grid-item icon="like" text="我的点赞" />
+        <van-grid-item icon="browsing-history" text="浏览历史" />
+      </van-grid>
+    </div>
 
     <!-- 设置，小智等 -->
     <van-cell-group>
       <van-cell title="消息通知" is-link />
       <van-cell title="用户反馈" is-link />
-      <van-cell title="小智同学" is-link to="/chat"/>
+      <van-cell title="小智同学" is-link to="/chat" />
       <van-cell title="系统设置" is-link />
     </van-cell-group>
   </div>
@@ -67,5 +80,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.user {
+  .nologin {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 30px;
+    .circle {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      text-align: center;
+      line-height: 100px;
+      color: #fff;
+      background-color: red;
+    }
+  }
+}
 </style>
