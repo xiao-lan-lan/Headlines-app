7f9746609de35b3ea8fa9f231df67077c37d98e4
兰小兰 <479323417@qq.com>
2019-12-09 18:23:37 +0800

    小智-对话渲染

--- src/views/robot/robot.vue
+++ src/views/robot/robot.vue
@@ -5,31 +5,64 @@
 
     <!-- 聊天列表 -->
     <div class="chat">
-      <div class="l">
+      <div :class="{l:!message.isMe,r:message.isMe}" v-for="(message,index) in messageList" :key="index">
         <van-image round width="40px" height="40px" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-        <span>哈哈哈</span>
+        <span>{{message.msg}}</span>
       </div>
-      <div class="r">
+      <!-- <div class="r">
         <span>嘿嘿嘿</span>
         <van-image round width="40px" height="40px" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-      </div>
+      </div> -->
     </div>
 
     <!-- 底部发送消息 -->
     <van-cell-group>
       <van-field v-model="userText" center clearable placeholder="小智等待您的输入哦">
-        <van-button slot="button" size="small" type="primary">发送</van-button>
+        <van-button slot="button" size="small" type="primary" @click="onSubmit">发送</van-button>
       </van-field>
     </van-cell-group>
   </div>
 </template>
 
 <script>
+import io from 'socket.io-client'
+import { setItem, getItem } from '@/utils/localStorage'
 export default {
   name: 'ChatPage',
   data () {
     return {
-      userText: ''
+      userText: '', // 输入内容
+      socket: null, // 通信对象
+      messageList: getItem('chat') || []// 消息列表
+    }
+  },
+  methods: {
+    onSubmit () {
+      // 发送消息
+      const data = {
+        msg: this.userText,
+        timestamp: Date.now(),
+        isMe: true
+      }
+      this.socket.emit('message', data)
+      this.messageList.push(data)
+      this.userText = ''
+    }
+  },
+  created () {
+    // 建立连接
+    this.socket = io('http://ttapi.research.itcast.cn')
+    // 接收消息
+    this.socket.on('message', data => {
+      console.log(data)
+      data.isMe = false
+      this.messageList.push(data)
+    })
+    // this.socket = socket
+  },
+  watch: {
+    messageList: function (newval, oldval) {
+      setItem('chat', newval)
     }
   }
 }
@@ -50,17 +83,22 @@ export default {
       background-color: #fff;
       border-radius: 10px;
       margin-left: 8px;
+      font-size: 16px;
+      white-space: wrap;
     }
     .l {
       display: flex;
       align-items: center;
+      margin: 12px 0;
     }
     .r {
       display: flex;
-      justify-content: flex-end;
+      flex-direction: row-reverse;
       align-items: center;
       span {
         margin-right: 8px;
+        background-color: #07c160;
+        color: #fff
       }
     }
   }
